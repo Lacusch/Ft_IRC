055b48aba195cf086c5c08be0e12dd6420fff354
Jide_O <45870109+JideOgunlana@users.noreply.github.com>
2023-09-11 12:14:16 +0000

    added _ops member to Channel class

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -1,137 +1,142 @@
 #include "../incl/Channel.hpp"
 
-Channel::Channel(std::string & name, std::string & password) 
-	:
-	_name(name),
-	_password(password),
-	_topic(""),
-	_limit(200),
-	_inviteOnly(off),
-	_topicMode(on), 
-	_passwordMode(on),
-	_userLimitMode(off){
-
-	Utils::print(B, "Men this -> " + this->_name + " is not easy");
-
+Channel::Channel(std::string& name, std::string& password)
+    : _name(name),
+      _password(password),
+      _topic(""),
+      _limit(200),
+      _inviteOnly(off),
+      _topicMode(on),
+      _passwordMode(on),
+      _userLimitMode(off) {
+    Utils::print(B, "Men this -> " + this->_name + " is not easy");
 }
 
-Channel::~Channel() {
-	
-}
+Channel::~Channel() {}
 
 // ------------------------------------------------------------
 // --------------------- Channel Methods ----------------------
 // ------------------------------------------------------------
 bool Channel::join(Client* client, int fd) {
+    if (isMember(client, fd)) return false;       // Client is already in the channel
+    if (_members.size() >= _limit) return false;  // Channel is full
 
-	if (isMember(client, fd))  return false; // Client is already in the channel
-	if (_members.size() >= _limit) return false; // Channel is full
+    _members[fd] = client;  // Add the client to the list of members
+	Utils::print(P, "Current size of channel: " + this->getName() + " -> ", 0);
+	std::cout << _members.size() << std::endl;
+    if (_members.size() == 1) {
+		_ops[this->getName()].push_back(client->getNickName());
+	}
+	Utils::print(Y, "Operators of channel: ");
+	for (std::map<std::string, std::vector<std::string> >::iterator it = this->_ops.begin(); it != this->_ops.end(); ++it) {
+		std::vector<std::string> savedOperators = it->second;
+		while (savedOperators.size() > 0) {
+			Utils::print(Y, "|" + it->first + "|" + " operator's name: " + "|" + savedOperators.back() + "|");
+			savedOperators.pop_back();
+		}
+	}
 
-	if (_members.size() == 0)
-		client->setOperator(true);
-
-	_members[fd] = client; // Add the client to the list of members
 	std::cout << "Client " << fd << " " << _members[fd]->getNickName() << " joined the channel." << std::endl;
 
 	return true;
 }
 
 bool Channel::kick(Client* client, int fd) {
-	if (!isMember(client, fd))  return false; // Client is not in the channel
-	_members.erase(fd); // Eject the client
-	return true;
+    if (!isMember(client, fd)) return false;  // Client is not in the channel
+    _members.erase(fd);                       // Eject the client
+    return true;
 }
 
 bool Channel::invite(Client* client, int fd) {
-
-	if (isMember(client, fd))  return false; // Client is already in the channel
-	return true; // Client is not in the channel
+    if (isMember(client, fd)) return false;  // Client is already in the channel
+    return true;                             // Client is not in the channel
 }
 
-bool Channel::leave(Client * client, int fd) {
+bool Channel::leave(Client* client, int fd) {
+    if (!isMember(client, fd)) return false;  // Client is not in the channel
+    _members.erase(fd);
+    return true;
+}
 
-	if (!isMember(client, fd))  return false; // Client is not in the channel
-	_members.erase(fd);
+bool Channel::modifyOpsPrivileges(const std::string& channel_name, const std::string& nickname, char flag) {
+	if (flag == '+') {
+		_ops[channel_name].push_back(nickname);
+	}
+	else if (flag == '-') {
+		_ops[channel_name].erase(std::remove(_ops[channel_name].begin(), _ops[channel_name].end(), nickname), _ops[channel_name].end());
+	}
 	return true;
 }
 
 // ------------------------------------------------------------
 // ------------------------- Setters --------------------------
 // ------------------------------------------------------------
-bool Channel::setTopic(const std::string & topic) {
-	if (isTopicValid(topic)) {
-		if (_topicMode == off) return false;
-		_topic = topic;
-		return true;
-	}
-	return false;
+bool Channel::setTopic(const std::string& topic) {
+    if (isTopicValid(topic)) {
+        if (_topicMode == off) return false;
+        _topic = topic;
+        return true;
+    }
+    return false;
 }
 
-bool Channel::setPassword(const std::string & password) {
-	if (_passwordMode == off) return false;
-	_password = password;
-	return true;
+bool Channel::setPassword(const std::string& password) {
+    if (_passwordMode == off) return false;
+    _password = password;
+    return true;
 }
-
 
 bool Channel::setUserLimit(unsigned int limit) {
-	if (_userLimitMode == off) return false;
+    if (_userLimitMode == off) return false;
 
-	if (limit < 2 || limit > 200) return false;
-	_limit = limit;
-	return true;
+    if (limit < 2 || limit > 200) return false;
+    _limit = limit;
+    return true;
 }
 
 // ------------------------------------------------------------
 // ----------------------- Mode Setters -----------------------
 // ------------------------------------------------------------
 
-void Channel::setPasswordMode(State mode) {
-	_passwordMode = mode;
-}
-void Channel::setTopicMode(State mode) {
-	_topicMode = mode;
-}
+void Channel::setPasswordMode(State mode) { _passwordMode = mode; }
+void Channel::setTopicMode(State mode) { _topicMode = mode; }
 
-void Channel::setInviteOnlyMode(State mode) {
-	_inviteOnly = mode;
-}
+void Channel::setInviteOnlyMode(State mode) { _inviteOnly = mode; }
 
-void Channel::setUserLimitMode(State mode) {
-	_userLimitMode = mode;
-}
+void Channel::setUserLimitMode(State mode) { _userLimitMode = mode; }
 
 // ------------------------------------------------------------
 // ------------------------- Getters --------------------------
 // ------------------------------------------------------------
-const std::string & Channel::getName() const {
-	return _name;
-}
+const std::string& Channel::getName() const { return _name; }
 
-const std::string & Channel::getTopic() const { return _topic; }
+const std::string& Channel::getTopic() const { return _topic; }
 
-const std::string & Channel::getPassword() const { return _password; }
+const std::string& Channel::getPassword() const { return _password; }
 
 unsigned int Channel::getLimit() const { return _limit; }
 
 bool Channel::isInviteOnly() const { return _inviteOnly; }
 
 void Channel::getMembers() {
-	std::map<int, Client*>::iterator it;
-	for (it = _members.begin(); it != _members.end(); ++it) {
-		Client* client = it->second;
-		std::cout << it->second << "---------->" << client->getNickName() << std::endl;
-	}
+    std::map<int, Client*>::iterator it;
+    for (it = _members.begin(); it != _members.end(); ++it) {
+        Client* client = it->second;
+        std::cout << it->second << "---------->" << client->getNickName() << std::endl;
+    }
 }
 
+std::map<int, Client*> Channel::getMembersList(void) { return (_members); }
 
 // ------------------------------------------------------------
 // -------------------------- Utils ---------------------------
 // ------------------------------------------------------------
-bool Channel::isMember(Client* client, int fd) const { return (_members.find(fd) != _members.end() && _members.at(fd) == client);}
+bool Channel::isMember(Client* client, int fd) const {
+    return (_members.find(fd) != _members.end() && _members.at(fd) == client);
+}
 
-bool Channel::isTopicValid(const std::string & topic) {
-	// check if topic has no line breaks
-	if (topic.find('\n') != std::string::npos) return false;
-	return true;
+bool Channel::isTopicValid(const std::string& topic) {
+    // check if topic has no line breaks
+    if (topic.find('\n') != std::string::npos) return false;
+    return true;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
+#include "../incl/Channel.hpp"
+#include "../incl/Client.hpp"
 #include "../incl/Request.hpp"
 #include "../incl/Server.hpp"
 #include "../incl/Utils.hpp"
 
-
-#include "../incl/Channel.hpp"
-#include "../incl/Client.hpp"
-
 #define SERVER_NAME "mr.server.com"
 #define SERVER_VERSION "v1.0.0"
 
-int main(int argc, char **argv) {
-
-/*     std::string channelName = "|--Test Jurassic Channel--|";
+int channel_playground(void) {
+    std::string channelName = "Jurassic Age";
     std::string password = "";
     Channel general(channelName, password);
 
-    Client client1(1); // Assuming file descriptor 1
-    Client client2(2); // Assuming file descriptor 2
-    Client client3(3); // Assuming file descriptor 3
+    std::string ch2 = "Discovery World";
+    std::string ch2Pass = "";
+    Channel discovery(ch2, ch2Pass);
+
+    Client client1(1);  // Assuming file descriptor 1
+    client1.setNickName("Client1");
+    Client client2(2);  // Assuming file descriptor 2
+    client2.setNickName("client2");
+    Client client3(3);  // Assuming file descriptor 3
+    client3.setNickName("client3");
+    Client client4(4);  // Assuming file descriptor 4
+    client4.setNickName("client4");
+    Client client5(5); // Assuming file descriptor 5
+    client5.setNickName("client5");
 
     if (general.join(&client1, 1)) {
         std::cout << "Client in " + general.getName() + " !" << std::endl;
@@ -27,7 +35,7 @@ int main(int argc, char **argv) {
     }
 
     general.setUserLimitMode(on);
-    general.setUserLimit(2);
+    // general.setUserLimit(2);
 
     if (general.join(&client3, 3)) {
         std::cout << "Client in " + general.getName() + " !" << std::endl;
@@ -45,6 +53,8 @@ int main(int argc, char **argv) {
 
     general.setUserLimit(120);
 
+
+    Utils::print(R, general.getName() + " channel properties: ");
     Utils::print(Y, "Topic: |" + general.getTopic() + "|");
     Utils::print(G, "Password: |" + general.getPassword() + "|");
     Utils::print(P, "User Limit: ", 0);
@@ -52,9 +62,52 @@ int main(int argc, char **argv) {
     Utils::print(G, "Invite Only: ", 0);
     std::cout << general.isInviteOnly() << std::endl;
 
-    return 0; */
-    if (argc != 3) return (Utils::print_error(INVALID_ARGC));
-    Server server(SERVER_NAME, SERVER_VERSION, argv[1], argv[2]);
-    if (server.valid_args() || server.start_server()) return (1);
-    server.run();
+    std::cout << std::endl;
+
+    Utils::print(R, discovery.getName() + " channel properties: ");
+    Utils::print(Y, "Topic: |" + discovery.getTopic() + "|");
+    Utils::print(G, "Password: |" + discovery.getPassword() + "|");
+    Utils::print(P, "User Limit: ", 0);
+    std::cout << discovery.getLimit() << std::endl;
+    Utils::print(G, "Invite Only: ", 0);
+    std::cout << discovery.isInviteOnly() << std::endl;
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    general.modifyOpsPrivileges(channelName, "Client2", '+'); // Gives Client2 operator privileges
+
+    if (general.join(&client4, 4)) {
+        std::cout << "Client in " + general.getName() + " !" << std::endl;
+    }
+
+    general.modifyOpsPrivileges(channelName, "Client1", '-'); // Takes back operator privileges from client1
+
+    if (general.join(&client5, 5)) {
+        std::cout << "Client in " + general.getName() + " !" << std::endl; 
+    }
+
+    std::cout << std::endl;
+    std::cout << std::endl;
+
+    if (discovery.join(&client5, 5)) {
+        std::cout << "Client in " + discovery.getName() + " !" << std::endl; 
+    }
+    if (discovery.join(&client1, 1)) {
+        std::cout << "Client in " + discovery.getName() + " !" << std::endl; 
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+
+    (void) argc;
+    (void) argv;
+
+    channel_playground();
+    // if (argc != 3) return (Utils::print_error(INVALID_ARGC));
+    // Server server(SERVER_NAME, SERVER_VERSION, argv[1], argv[2]);
+    // if (server.valid_args() || server.start_server()) return (1);
+    // server.run();
 };
